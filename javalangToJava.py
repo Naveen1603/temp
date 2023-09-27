2b2c00c03f11c5ac905e5d7310d9361738ec8ce7
Sambangi Naveen <54176509+Naveen1603@users.noreply.github.com>
2023-09-27 19:21:50 +0530

    Update javalangToJava.py

--- javalangToJava.py
+++ javalangToJava.py
@@ -1,16 +1,22 @@
-javafrom javalang.tree import *
+from javalang.tree import *
 
 # Assuming you have the root node of the AST named "root"
 
-def dump_ast_to_file(node, writer):
-    if isinstance(node, CompilationUnit):
-        for child in node.types:
-            dump_ast_to_file(child, writer)
-    elif isinstance(node, TypeDeclaration):
-        writer.write(node._position.source)
-    elif isinstance(node, MethodDeclaration):
-        writer.write(node._position.source)
+def dump_ast_to_file(node, indent_level, writer):
+    indent = "    " * indent_level
+    writer.write(f"{indent}{node._node_type}\n")
+
+    if isinstance(node, Node):
+        for child_name, child_value in node.children:
+            writer.write(f"{indent}  {child_name}: ")
+            if isinstance(child_value, list):
+                writer.write("[\n")
+                for child in child_value:
+                    dump_ast_to_file(child, indent_level + 2, writer)
+                writer.write(f"{indent}  ]\n")
+            else:
+                dump_ast_to_file(child_value, indent_level + 1, writer)
 
 # Write the AST to a Java code file
 with open("MyClass.java", "w") as file:
-    dump_ast_to_file(root, file)
+    dump_ast_to_file(root, 0, file)
